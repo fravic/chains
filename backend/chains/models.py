44dd927ba0cfcd90aad8b5f3257ff1bc6ab9acad
Julian Pulgarin <jp@julianpulgarin.com>
2013-02-25 02:18:44 -0500

    Added payment amount and referee email to chain model

--- backend/chains/models.py
+++ backend/chains/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 class Chain(models.Model):
     name = models.CharField(max_length=255)
+    stakes = models.PositiveIntegerField()
+    referee_email = models.EmailField()
 
 class X(models.Model):
     chain = models.ForeignKey(Chain)
