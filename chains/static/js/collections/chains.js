63a302365750c86c1063735528b9f3eba3d2c5cc
Fravic <fravicf@gmail.com>
2013-02-25 05:08:04 -0500

    change to relative path

--- chains/static/js/collections/chains.js
+++ chains/static/js/collections/chains.js
@@ -5,6 +5,6 @@ $(function($) {
 
     app.ChainSet = Backbone.Collection.extend({
         model: app.Chain,
-        url: 'http://dontbreakthechain.herokuapp.com/api/v1/chain',
+        url: '/api/v1/chain',
     });
 });
