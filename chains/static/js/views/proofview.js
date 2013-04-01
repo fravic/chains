ad8ffb4fccfea8752b8be6e31ef7f535e7d9d6f2
Fravic <fravicf@gmail.com>
2013-04-01 03:13:11 -0400

    try camera again

--- chains/static/js/views/proofview.js
+++ chains/static/js/views/proofview.js
@@ -7,31 +7,9 @@ $(function($) {
         initialize: function(a) {
             _.bindAll(this);
 
-            try {
-                blackberry.media.camera.takePicture(successCB, closedCB, errorCB);
-            } catch(e) {
-                alert("Error in supported: " + e);
-            }
+            takePicture();
+
             document.getElementById('upload').addEventListener('click', this.upload, false);
-        },
-
-        displayImage: function(evt) {
-            files = evt.target.files; // FileList object
-
-            // Loop through all Images
-            for (var i = 0, f; f = files[i]; i++) {
-                var reader = new FileReader();
-                // Closure to capture the file information.
-                reader.onload = (function(theFile) {
-                    return function(e) {
-                        // Display Image
-                        document.getElementById('photocontain').innerHTML = ['<img class="imageDisp" src="', e.target.result,
-                                                                     '" title="', escape(theFile.name), '"width="280" height="440"/>'].join('');//Modify Image size as needed
-                    };
-                })(f);
-                // Read in the image file as a data URL.           
-                reader.readAsDataURL(f);
-          }
         },
 
         upload: function() {
